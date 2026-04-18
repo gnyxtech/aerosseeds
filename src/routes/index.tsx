0c433d57a5e7c25450e02b1b8fad8d74dc64588c
yashwant-kargwal <yashwantkargwal9@gmail.com>
2026-04-19 01:55:54 +0530

    feat(product) : add product page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ import { PATH_DASHBOARD } from './paths';
 import { About } from '../pages/about/about';
 import Contact from './../pages/contact/contact';
 import NotFound from '../pages/404page/pageNotFound';
+import { Products } from '../pages/product/products';
 
 // ----------------------------------------------------------------------
 
@@ -19,7 +20,7 @@ export function Router() {
         { path: PATH_DASHBOARD.home, element: <HomePage /> },
         { path: PATH_DASHBOARD.aboutUs, element: <About /> },
         { path: PATH_DASHBOARD.contactUs, element: <Contact /> },
-        { path: PATH_DASHBOARD.products, element: <About /> },
+        { path: PATH_DASHBOARD.products, element: <Products /> },
       ],
     },
     { path: '*', element: <NotFound /> },
